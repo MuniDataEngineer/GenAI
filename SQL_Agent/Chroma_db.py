bd4fb1edcee11c6cc8d18f7e53a1b93bc87fb40b
Muniyaraj M <135152922+MuniDataEngineer@users.noreply.github.com>
2025-06-13 18:41:37 +0530

    Create Chroma_db.py

---
+++ SQL_Agent/Chroma_db.py
@@ -0,0 +1,25 @@
+import chromadb
+from chromadb.config import Settings
+from functions import *
+from schema_list import schema
+
+#configuring vectordb:
+print("Creating Chroma Database...")
+client = chromadb.PersistentClient(path=".GenAI/SQL_Agent/chroma_schema_db")
+collection = client.get_or_create_collection(name="schema_collection")  #you can change the name of the collection
+
+#model(embeddings):
+model = 'models/embedding-001'
+
+#schema:
+schema = schema
+
+#schema into vectors:
+schema_vect = vector_embedding(schema,model,genai)
+schema_ids = vector_Id(schema)
+schema_meta_data = vector_metadata(schema)
+
+#Load schema vector into chroma db:
+print("loading your schema details into chroma db....")
+vector_load(schema,schema_vect,schema_ids,schema_meta_data,collection)
+print("Completed...")
