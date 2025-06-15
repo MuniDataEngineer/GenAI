9b6ac4f6d4f1b47c0c23d33123bcac42196d24a1
Muniyaraj M <135152922+MuniDataEngineer@users.noreply.github.com>
2025-06-15 21:06:01 +0530

    Create snowflake.py

---
+++ SQL_Agent/snowflake.py
@@ -0,0 +1,12 @@
+import snowflake.connector
+
+# Connect to your Snowflake account
+def connection_snowflake(user,password,account,database,schema,role):
+  conn = snowflake.connector.connect(
+      user=user,
+      password=password,
+      account=account,       
+      database=database,
+      schema=schema,
+      role=role
+  )
