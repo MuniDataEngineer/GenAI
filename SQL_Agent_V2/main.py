bfad9bee8e59388c0a3c1a9282f4c0950542ca2b
Muniyaraj M <135152922+MuniDataEngineer@users.noreply.github.com>
2025-06-29 18:11:55 +0530

    Update main.py

--- SQL_Agent_V2/main.py
+++ SQL_Agent_V2/main.py
@@ -2,7 +2,8 @@ from snowflake_connection import connection
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 from schema_list import schema 
-from SQL_Agent_V2/chroma_db.py import create_vector_store
+from chroma_db.py import create_vector_store
+from langgraph_orchestration import orchestration
 import getpass
 
 
@@ -35,6 +36,19 @@ schema = schema
 
 #chromadb setup
 vector_store = create_vector_store(schema,embedding_model) 
+
+#langgraph compile
+graph = graph
+
+#Invoke the chatmodel with questions
+print("Hi there Now I can answer your question..\n")
+question = input("What Can I help you today..\n")
+
+result = graph.invoke({"question": question })
+
+print(result["generated_sql"])      # See the generated SQL
+print(result["query_result"])       # Final result from Snowflake
+
 
 
 
