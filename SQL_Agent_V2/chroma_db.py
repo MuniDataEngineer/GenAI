850c84d0f9338d03afaa6f9b163bd6eaa3cbec0b
Muniyaraj M <135152922+MuniDataEngineer@users.noreply.github.com>
2025-06-29 17:21:21 +0530

    Create chroma_db.py

---
+++ SQL_Agent_V2/chroma_db.py
@@ -0,0 +1,14 @@
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+from uuid import uuid4
+
+def create_vector_store(schemas: list[str]):
+  docs = [Document(page_content=s, metadata={"source": "schema"}) for s in schemas]
+  uuids = [str(uuid4()) for _ in range(len(docs))]
+  vector_store = Chroma(
+      collection_name="example_collection",
+      embedding_function=embeddings,
+      persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not necessary
+  )
+  vector_store.add_documents(documents=docs, ids=uuids)
+  return vector_store
