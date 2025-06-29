f711bda38212beaaf20f938f9438a1f684591733
Muniyaraj M <135152922+MuniDataEngineer@users.noreply.github.com>
2025-06-29 19:23:13 +0530

    Update nodes.py

--- SQL_Agent_V2/nodes.py
+++ SQL_Agent_V2/nodes.py
@@ -1,10 +1,19 @@
 from langchain_chroma import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from snowflake_connection import connection
+from prompt import prompt
+import os
+
+embedding_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key= os.environ["GOOGLE_API_KEY"])
+snow_conn = connection(os.environ["USER"],os.environ["PASSWORD"],os.environ[""],os.environ["ACCOUNT"],os.environ["DATABASE"],os.environ["SCHEMA"],os.environ["ROLE"])
+sql_chain = prompt()
+
 
 def retrieve_schema_node(state):
     vectordb = Chroma(
         collection_name="example_collection",
         persist_directory="./chroma_langchain_db",
-        embedding_function=embeddings,
+        embedding_function=embedding_model,
     )
     query = state["question"]
     doc = vectordb.similarity_search(query, k=2)
@@ -22,7 +31,7 @@ def generate_sql_node(state):
 
 def execute_sql_node(state):
     sql = state["generated_sql"]
-    conn = conn
+    conn = snow_conn
     cursor = conn.cursor()
     cursor.execute(sql)
     result = cursor.fetchall()
