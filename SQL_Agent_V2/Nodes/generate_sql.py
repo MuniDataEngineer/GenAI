0e8d021ba0ffd32e161967be88d34c12d39a4513
Muniyaraj M <135152922+MuniDataEngineer@users.noreply.github.com>
2025-06-29 18:15:16 +0530

    Delete SQL_Agent_V2/Nodes directory

--- SQL_Agent_V2/Nodes/generate_sql.py
+++
@@ -1,6 +0,0 @@
-def generate_sql_node(state):
-    result = sql_chain.invoke({
-        "schema_context": state["schema_context"],
-        "question": state["question"]
-    })
-    return {"generated_sql": result.content}
